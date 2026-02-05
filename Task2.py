f180f44c18fe37b6a811fcba30f45bd352377440
Camila Richter <ric23183@uvg.edu.gt>
2026-02-04 22:43:16 -0600

    creación de archivos

---
+++ Task2.py
@@ -0,0 +1,1 @@
+#Camila Y Marinés
---
+++ Task3.py
@@ -0,0 +1,8 @@
+#Camila y Marinés
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("bobbyscience/league-of-legends-diamond-ranked-games-10-min")
+
+print("Path to dataset files:", path)
